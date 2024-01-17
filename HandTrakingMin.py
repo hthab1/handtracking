8b1a12a730f35a5caf433975e452f55d65e950fe
Habteweld Yimer <ht@Habtewelds-MacBook-Pro.local>
2024-01-17 22:37:34 +0300

    chanched it into module

--- HandTrakingMin.py
+++ HandTrakingMin.py
@@ -25,7 +25,7 @@ while True:
                 h,w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 if id == 0:
-                    cv2.circle(img, (cx, cy), 25, (255,0,255),cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255,0,255),cv2.FILLED)
                 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
